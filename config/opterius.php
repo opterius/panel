c284b4ba9fab77112c22267f2bb6933218285afe
Gabriel Chimilevschi <gabriel1978@gmail.com>
2026-04-29 17:46:52 +0300

    release: v2.8.3

--- config/opterius.php
+++ config/opterius.php
@@ -83,7 +83,7 @@ return [
         'rate_limit' => env('OPTERIUS_API_RATE_LIMIT', 60),
     ],
 
-    'version' => '2.8.2',
+    'version' => '2.8.3',
     'license_key' => env('OPTERIUS_LICENSE_KEY', ''),
     'license_server_url' => env('OPTERIUS_LICENSE_URL', 'https://opterius.com'),
 
