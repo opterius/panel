a2b54214547a9edad7bacc5a36982b3a37f4d996
Gabriel Chimilevschi <gabriel1978@gmail.com>
2026-05-20 14:11:02 +0300

    release: v2.9.10

--- config/opterius.php
+++ config/opterius.php
@@ -87,7 +87,7 @@ return [
     // Must match OPTERIUS_SSO_SECRET on opterius.com.
     'sso_secret' => env('OPTERIUS_SSO_SECRET'),
 
-    'version' => '2.9.9',
+    'version' => '2.9.10',
     'license_key' => env('OPTERIUS_LICENSE_KEY', ''),
     'license_server_url' => env('OPTERIUS_LICENSE_URL', 'https://opterius.com'),
 
